fcd4964f7529dc65683bdbc8e88adf44e4ab29c8
Adrian Zankich <adzankich@gmail.com>
2015-06-09 14:22:10 -0700

    Update mjpeg stream example and add opencv face tracking example

--- examples/mjpeg-stream.js
+++ examples/mjpeg-stream.js
@@ -5,14 +5,35 @@ var bebop = require("../."),
 
 var drone = bebop.createClient(),
     mjpg = drone.getMjpegStream(),
-    s = new cv.ImageStream(),
+    buf = null,
     w = new cv.NamedWindow("Video", 0);
 
-mjpg.pipe(s);
+drone.connect();
 
-s.on("data", function(matrix) {
-  w.show(matrix);
-  w.blockingWaitKey(0, 10);
+
+mjpg.on("data", function(data) {
+  buf = data;
 });
 
-drone.connect();
+setInterval(function() {
+  if (buf == null) {
+    return;
+  }
+
+  try {
+    cv.readImage(buf, function(err, im) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (im.width() < 1 || im.height() < 1) {
+          console.log("no width or height");
+          return;
+        }
+        w.show(im);
+        w.blockingWaitKey(0, 50);
+      }
+    });
+  } catch(e) {
+    console.log(e);
+  }
+}, 100);
---
+++ examples/opencv-face-tracking.js
@@ -0,0 +1,55 @@
+"use strict";
+
+//
+// This example requires the haarcascade file located at
+// https://github.com/peterbraden/node-opencv/raw/master/data/haarcascade_frontalface_alt.xml
+//
+
+var bebop = require("../."),
+    cv = require("opencv"),
+    async = require("async");
+
+var drone = bebop.createClient(),
+    mjpg = drone.getMjpegStream(),
+    w = new cv.NamedWindow("Video", 0),
+    buf = null;
+
+drone.connect();
+
+mjpg.on("data", function(data) {
+  buf = data;
+});
+
+async.forever(
+  function(callback) {
+    if (buf == null) {
+      return callback();
+    }
+
+    cv.readImage(buf, function(err, im) {
+      if (err) {
+        console.log(err);
+        callback();
+      } else {
+        im.detectObject("haarcascade_frontalface_alt.xml", {}, function(e, faces) {
+          if (e) {
+            console.log(err);
+          }
+
+          for (var i = 0; i < faces.length; i++) {
+            var face = faces[i];
+            im.rectangle([face.x, face.y],
+              [face.x + face.width, face.y + face.height], [0, 255, 0], 2);
+          }
+
+          w.show(im);
+          w.blockingWaitKey(0, 50);
+
+          setTimeout(function() {
+            callback();
+          }, 100);
+        });
+      }
+    });
+  }
+);
