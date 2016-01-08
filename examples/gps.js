e0ae59af9c825068592ab7fb226096477c2680d7
deadprogram <ron@hybridgroup.com>
2016-01-07 19:18:36 -0800

    Make ESLint happy

--- examples/gps.js
+++ examples/gps.js
@@ -5,33 +5,33 @@ var bebop = require("../.");
 var drone = bebop.createClient();
 
 drone.connect(function() {
-  drone.on("ready", function(){
+  drone.on("ready", function() {
     console.log("ready");
   });
 
-  drone.on("GPSFixStateChanged", function(data){
+  drone.on("GPSFixStateChanged", function(data) {
     console.log("GPSFixStateChanged", data);
-  })
+  });
 
-  drone.on("GPSUpdateStateChanged", function(data){
+  drone.on("GPSUpdateStateChanged", function(data) {
     console.log("GPSUpdateStateChanged", data);
   });
 
-  drone.on("GPSSettingsState", function(data){
+  drone.on("GPSSettingsState", function(data) {
     console.log("GPSSettingsState", data);
   });
 
-  drone.on("GPSState", function(data){
+  drone.on("GPSState", function(data) {
     console.log("GPSState", data);
   });
 
-  drone.on("flying", function(){
+  drone.on("flying", function() {
     console.log("flying");
   });
 
-  drone.on("landing", function(){
+  drone.on("landing", function() {
     console.log("landing");
   });
 
-  drone.GPSSettings.ResetHome()
+  drone.GPSSettings.resetHome();
 });
