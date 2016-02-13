030ce3e08661dc3f1c87703b961d2bfad5db9e24
deadprogram <ron@hybridgroup.com>
2016-02-13 09:02:31 -0800

    Additional examples for 'full' API use

---
+++ examples/events.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var bebop = require("../.");
+
+var drone = bebop.createClient();
+
+drone.connect(function() {
+  drone.on("ready", function() {
+    console.log("ready");
+  });
+
+  drone.on("battery", function(data) {
+    console.log(data);
+  });
+
+  drone.on("landed", function() {
+    console.log("landed");
+  });
+
+  drone.on("takingOff", function() {
+    console.log("takingOff");
+  });
+
+  drone.on("hovering", function() {
+    console.log("hovering");
+  });
+
+  drone.on("flying", function() {
+    console.log("flying");
+  });
+
+  drone.on("landing", function() {
+    console.log("landing");
+  });
+
+});
