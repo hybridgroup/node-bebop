3a896753ad274852da880e70e3719f3d6bb78f6d
deadprogram <ron@hybridgroup.com>
2016-12-26 09:10:57 +0100

    Update location for XML files with Parrot command sets
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- bin/updateCommands.js
+++ bin/updateCommands.js
@@ -5,27 +5,26 @@ var fs = require('fs');
 
 
 var ARDroneCommands = new Promise(function (resolve, reject) {
-  request('https://raw.githubusercontent.com/Parrot-Developers/libARCommands/master/Xml/ARDrone3_commands.xml', function (err, res, xml) {
+  request('https://raw.githubusercontent.com/Parrot-Developers/arsdk-xml/master/xml/ardrone3.xml', function (err, res, xml) {
     resolve(parser.toJson(xml));
   })
 })
 
 var CommonCommands = new Promise(function (resolve, reject) {
-  request('https://raw.githubusercontent.com/Parrot-Developers/libARCommands/master/Xml/common_commands.xml', function (err, res, xml) {
+  request('https://raw.githubusercontent.com/Parrot-Developers/arsdk-xml/master/xml/common.xml', function (err, res, xml) {
     resolve(parser.toJson(xml));
   })
 })
 
 
 Promise.all([ARDroneCommands, CommonCommands]).then(function (json) {
-  
+
   var arr = json.map(function (json) {
     return JSON.parse(json).project;
   })
-  
+
   fs.writeFile('./lib/commands.json', JSON.stringify(arr), function (err) {
     console.log(err);
   })
-  
-})
 
+})
